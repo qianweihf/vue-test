fc5acbe5f809fe9606f36012cf34a09fd428277f
qianweihf <qianweihf@126.com>
2023-02-20 23:24:49 +0800

    map相关

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,8 +2,11 @@
   <div>
     <h1>人员列表</h1>
     <h3 style="color:red">Count组件的求和结果:{{sum}}</h3>
+    <h3>人员列表中第一个人的名字是：{{firstPersonName.name}}</h3>
     <input type="text" placeholder="请输入姓名" v-model="name">
     <button @click="addPerson">添加</button>
+    <button @click="addPersonWang">添加一个姓王的人</button>
+    <button @click="addPersonServer">添加一个人，名字随机</button>
     <ul>
       <li v-for="p in personList" :key="p.id">{{p.name}}</li>
     </ul>
@@ -25,6 +28,9 @@
       },
       sum(){
         return this.$store.state.countAbout.sum
+      },
+      firstPersonName(){
+        return this.$store.getters["personAbout/firstPersonName"]
       }
     },
     methods:{
@@ -32,6 +38,13 @@
         const personObj={id:nanoid(),name:this.name}
         this.$store.commit('personAbout/ADD_PERSON',personObj)
         this.name=''
+      },
+      addPersonWang(){
+        const personObj={id:nanoid(),name:this.name}
+        this.$store.dispatch('personAbout/addPersonWang',personObj)
+      },
+      addPersonServer(){
+        this.$store.dispatch('personAbout/addPersonServer')
       }
     }
   }
--- src/store/index.js
+++ src/store/index.js
@@ -2,60 +2,14 @@
 
 //引入vuex
 import Vuex from 'vuex'
+
+import countOptions from './modules/count'
+
+import personOptions from './modules/person'
 //引入Vue
 import Vue from 'vue'
 //使用插件
 Vue.use(Vuex)
-
-const countOptions = {
-    namespaced:true,
-    actions:{
-        jiaOdd(context,value){
-            if(context.state.sum % 2){
-                context.commit('JIA',value);
-            }
-        },
-        jiaWait(context,value){
-            setTimeout(()=>{
-                context.commit('JIA',value);
-            },500)
-        }
-    },
-    mutations:{
-        JIA(state,value){
-            state.sum += value;
-        },
-        JIAN(state,value){
-            state.sum -= value;
-        },
-    },
-    state:{
-        sum:0,  //当前的和
-        school:'尚硅谷',
-        subject:'前端',
-    },
-    getters:{
-        bigSum(state){
-            return state.sum * 10;
-        }
-    }
-}
-
-const personOptions = {
-    namespaced:true,
-    actions:{},
-    mutations:{
-        ADD_PERSON(state,value){
-            state.personList.unshift(value);
-        }
-    },
-    state:{
-        personList:[
-            {id:'001',name:'张三'}
-        ]
-    },
-    getters:{}
-}
 
 //创建并暴露store
 export default new Vuex.Store({
