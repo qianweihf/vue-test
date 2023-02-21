93f10a80979f6b93be6a937e1a3c13bffb2482e4
qianweihf <qianweihf@126.com>
2023-02-21 23:25:03 +0800

    vuex模块化

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>我是home的内容</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Home'
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-//引入vue-resource插件
-import vueResource from 'vue-resource'
-//引入store
-import store from './store'
+
+//引入vue-router
+import VueRouter from 'vue-router'
+import router from './router'
 
 Vue.config.productionTip = false
-//使用vue-resource插件
-Vue.use(vueResource)
 
+//使用插件
+Vue.use(VueRouter)
 
 new Vue({
     el: '#app',
     render: h => h(App),
-    store,
-    beforeCreate() {
-        Vue.prototype.$bus = this;
-    }
+    router,
 })
